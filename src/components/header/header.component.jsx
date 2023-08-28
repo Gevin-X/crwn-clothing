a084764618a57577d43e841ad58960c7c87bc790
gevin-x <gemba.gp@gmail.com>
2023-08-28 09:35:34 +0530

    add cartIcon and imlement cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { ReactComponent as Logo} from "../../assets/crown.svg";
 import './header.style.scss'
 import { auth } from "../../firebase/firebase.utils";
 import {  connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header= ({currentUser}) =>(
+const Header= ({currentUser, hidden}) =>(
     <div className="header">
         <Link className='logo-container' to='/'>
             <Logo className="logo"></Logo>
@@ -16,7 +18,7 @@ const Header= ({currentUser}) =>(
             </Link>
             <Link className='option' to='/contact'>
                 CONTACT
-            </Link>
+            </Link>           
             {currentUser ? (
                 <div className="option" onClick={() => auth.signOut()}>
                     SIGN OUT 
@@ -26,13 +28,15 @@ const Header= ({currentUser}) =>(
                     SIGN IN
                 </Link>
             )}
-
+            <CartIcon/>            
         </div>
+        {hidden ? null : <CartDropdown/>}
     </div>
 ) 
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
